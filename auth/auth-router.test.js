dabe28f59db70ced19e3f0226368bea1432dea9d
Suzanne Cabral <suzannecabral@live.com>
2020-11-13 10:22:59 -0800

    Sanity check passed

--- auth/auth-router.test.js
+++ auth/auth-router.test.js
@@ -1,28 +1,13 @@
-const supertest = require('supertest');
+const request = require('supertest');
 const router = require('./auth-router');
 
 //sanity check, connected
 
-// describe('router', ()=>{
-//   it('runs the tests', ()=>{
-//     expect(true).toBe(true);
-//   });
-// });
-
-//always test for the http status code
-
-describe('GET /', ()=>{
-  it('should respond with 200', ()=>{
-    supertest(router).get('/').expect(500);
+describe('auth router module', ()=>{
+  it('is the testing environment', ()=>{
+    expect(process.env.DB_ENV).toBe('testing');
+    expect(process.env.DB_ENV).not.toBe('production');
+    expect(process.env.DB_ENV).not.toBe('development');
   });
 });
 
-// describe('GET /', function (){
-//   it('should respond with 200', function (){
-//     supertest(router)
-//       .get('/')
-//       .then(res=>{
-//         expect(res.status).toBe(500);
-//       });
-//   });
-// });
