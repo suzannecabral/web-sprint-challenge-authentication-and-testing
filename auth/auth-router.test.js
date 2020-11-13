2b5d37633f332f1cd72dc0c7e660271d4062c647
Suzanne Cabral <suzannecabral@live.com>
2020-11-13 10:18:00 -0800

    Adding dependencies for server testing

--- auth/auth-router.test.js
+++ auth/auth-router.test.js
@@ -1,9 +1,28 @@
+const supertest = require('supertest');
 const router = require('./auth-router');
 
-describe('router', ()=>{
-  it('runs the tests', ()=>{
-    expect(true).toBe(true);
-  });
-});
+//sanity check, connected
+
+// describe('router', ()=>{
+//   it('runs the tests', ()=>{
+//     expect(true).toBe(true);
+//   });
+// });
+
 //always test for the http status code
 
+describe('GET /', ()=>{
+  it('should respond with 200', ()=>{
+    supertest(router).get('/').expect(500);
+  });
+});
+
+// describe('GET /', function (){
+//   it('should respond with 200', function (){
+//     supertest(router)
+//       .get('/')
+//       .then(res=>{
+//         expect(res.status).toBe(500);
+//       });
+//   });
+// });
