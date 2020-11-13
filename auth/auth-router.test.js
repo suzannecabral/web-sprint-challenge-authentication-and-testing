5f15998eade6fe28b02cfd039e57924245aec282
Suzanne Cabral <suzannecabral@live.com>
2020-11-13 09:02:09 -0800

    updated test for router not server

--- auth/auth-router.test.js
+++ auth/auth-router.test.js
@@ -1,8 +1,9 @@
-const server = require('../api/server');
+const router = require('./auth-router');
 
-describe('server', ()=>{
+describe('router', ()=>{
   it('runs the tests', ()=>{
     expect(true).toBe(true);
   });
 });
 //always test for the http status code
+
